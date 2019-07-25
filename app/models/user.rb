a6399b91c7896fb154a4ee850d16111f55d92cff
ispirett <ispirett@gmail.com>
2019-07-25 12:01:26 -0400

    stash

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
-      user.avatar = auth.info.avatar
+      #user.name = auth.info.name
+      #user.avatar = auth.info.avatar
     end
   end
 
