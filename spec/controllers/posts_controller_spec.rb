ddf91098a892c03c77dc44afdfc6169ca0719544
ispirett <ispirett@gmail.com>
2019-07-30 18:47:40 -0400

    tests(rspec)

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  login_user
    context 'validates logic' do
-    it 'Check that we get all posts' do
-
+    it 'should redirect if now logged in' do
+      get :index
+      expect(response.status).to eq 200
     end
    end
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,6 @@
-
+require_relative 'controller_macros'
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, :type => :controller
 end
