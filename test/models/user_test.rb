2179be53307803ea6edfe2c7dfac4707b22784f5
SotoArmando <armando29@live.com>
2019-07-30 17:24:15 -0400

    feature(test): user test + template asserts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,14 @@ class UserTest < ActiveSupport::TestCase
     user = users(:michael)
     assert user.valid?
   end
+
+  test "login with valid information" do
+    get sign_in_users_path
+    assert_template 'sessions/new'
+    post sign_in_users_path, params: { session: { email: "i@gmail.com", password: "12345678" } }
+    assert_template 'sessions/new'
+    assert flash.empty?
+    get root_path
+    assert flash.empty?
+  end
 end
