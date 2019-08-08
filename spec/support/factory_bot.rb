15b73f35cd9e8dd93fe77a5ba87cc785a22f653f
SotoArmando <armando29@live.com>
2019-08-08 14:22:36 -0400

    feature(test): added rspect formatt

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,12 @@
+require_relative 'posts_support'
+require_relative 'likes_support'
+require 'capybara/rails'
 RSpec.configure do |config|
-    config.include FactoryBot::Syntax::Methods
-  end
+  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  #config.include RequestSpecHelper, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Capybara::DSL
+  config.include PostSupport
+  config.include LikeSupport
+end
